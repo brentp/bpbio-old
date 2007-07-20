ebd9c9b18fe11a5c45734bc6c22b4f2b4244148e
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2007-07-20 19:33:38 +0000

    fix naming of blastfile.

--- trunk/blasttools/splitblast/splitblast.py
+++ trunk/blasttools/splitblast/splitblast.py
@@ -1,23 +1,23 @@
-"""
-take a single blastfile of m chromosomes vs n chromosomes and split it
-into m * n files based on the queryfn, subjectfn functions which take
-a string accn and return the chromsome: e.g.
-"""
-def queryfn(accn):
-    """>>> queryfn('At2g26430')
-    '2'
-    """
-    return accn[2]
         
 
 def split_blast(blastfile,queryfn,subjectfn):
+    """
+    take a single blastfile of m chromosomes vs n chromosomes and split it
+    into m * n files based on the queryfn, subjectfn functions which take
+    a string accn and return the chromsome: e.g.
+    >>> queryfn('At2g26430')
+    '2'
+    >>> subjectfn('Os01g12345')
+    '01'
+    """
+
     outfiles = {}
     for line in open(blastfile):
         line = line.strip().split("\t")
         querychr = queryfn(line[0])
         subjectchr = subjectfn(line[1])
         key = str(querychr) + '_' + str(subjectchr)
-        filename = blastfile + '_' + key
+        filename = blastfile[:blastfile.rfind(".")] + '_' + key + '.blast'
         if not key in outfiles:
             outfiles[key] = open(filename,'w')
         print >>outfiles[key], "\t".join(line)
