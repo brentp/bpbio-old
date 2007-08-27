7d00a55106164990dcffd1cbf38ec66ff1d2405a
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2007-08-27 20:29:28 +0000

    check in changes to parser.

--- trunk/blasttools/splitblast/splitblast.py
+++ trunk/blasttools/splitblast/splitblast.py
@@ -16,22 +16,45 @@ def split_blast(blastfile,queryfn,subjectfn,qorg,sorg):
         subjectchr = subjectfn(line[1])
         qchr = str(querychr)
         schr = str(subjectchr)
+        if None in (qchr, schr):
+            continue
         key = qchr + '_' + schr
         filename = qorg + 'chr' + qchr + '_vs_' \
                  + sorg + 'chr' + schr + '.blast'
 
-        if not key in outfiles:
-            outfiles[key] = open(filename,'w')
-        print >>outfiles[key], "\t".join(line)
+        #if not key in outfiles:
+        #    outfiles[key] = open(filename,'w')
+        #print >>outfiles[key], "\t".join(line)
+        for qs in (0, 1):
+            if '.' in line[qs]:
+                line[qs] = line[qs][:line[qs].find('.')]
+
+        fh = open(filename,'a')
+        print >>fh, "\t".join(line)
+        fh.close()
 
 
 
 if __name__ == "__main__":
     import sys
+    import re
     #split_blast(sys.argv[1],lambda x: x[2:3], lambda x: x[1:3], qorg=sys.argv[2], sorg=sys.argv[3])
-    def qschr(accn):
-        re.search('Sb(\d+)',accn).groups(0)[0]
+    def sbchr(accn):
+        chr = re.search('SB(\d+)',accn).groups(0)[0]
+        if int(chr) > 10:
+            return None
+        else: 
+            return chr
+    def oschr(accn):
+        return re.search('OS(\d+)',accn).groups(0)[0]
 
-    split_blast(sys.argv[1], qschr, qschr, qorg=sys.argv[2], sorg=sys.argv[3])
+    
+    maizefn = lambda x: x.split('|')[1] 
+    #split_blast(sys.argv[1], lambda x: x[2:3], lambda x: x[2:3], qorg=sys.argv[2], sorg=sys.argv[3])
+    #split_blast(sys.argv[1], qschr, qschr, qorg=sys.argv[2], sorg=sys.argv[3])
+    #split_blast(sys.argv[1], lambda x: x.lstrip('>')[2:4], lambda x: x.lstrip('>')[2:4], qorg=sys.argv[2], sorg=sys.argv[3])
+
+    #split_blast(sys.argv[1], maizefn, maizefn, qorg=sys.argv[2], sorg=sys.argv[3])
+    split_blast(sys.argv[1], sbchr, sbchr, qorg=sys.argv[2], sorg=sys.argv[3])
 
  
