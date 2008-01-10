ba198d091d0f1a52227cbda91a0bd11cba136899
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2008-01-10 01:56:56 +0000

    add data, and failing blast file

--- blasttools/blast_misc/test_b.py
+++ blasttools/blast_misc/test_b.py
@@ -1,12 +1,8 @@
-import blast_misc
 import sys
+import blast_misc
 import time
 import operator
 
-#blast_file = '/tmp/all.blast'
-blast_file = '/tmp/arabidopsischr5_arabidopsischr5.blast'
-t0 = time.time()
+blast_file = 'data/t.blast'
 b = blast_misc.blast_array(blast_file, dopickle=0)
-t1 = time.time()
-print t1 - t0
-print sorted(b[b['query'] == 'AT5G67620'], key=operator.itemgetter('subject'))
+print b[:10]
