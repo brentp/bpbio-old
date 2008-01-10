940e6cc75149f033d96d05f361f488328d187f5c
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2008-01-10 01:56:56 +0000

    add data, and failing blast file

--- trunk/blasttools/blast_misc/blast_misc.pyx
+++ trunk/blasttools/blast_misc/blast_misc.pyx
@@ -106,6 +106,7 @@ def _blast_array(blastf, maxkeep):
 
 def blast_array(fi, dopickle=1, best_hit=1, maxkeep=6):
     file_name = 0
+    if not os.path.exists(fi): return []
     if not isinstance(fi, list) and type(fi) != numpy.recarray:
         file_name = fi + ".pickle"
 
