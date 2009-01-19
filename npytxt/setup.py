3880f3883e5de9f68b19627f817bf34f26cfa9fa
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-01-19 23:46:04 +0000

    rename dir

---
+++ npytxt/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(name          = 'npytxt',
+      version       = '0.1.0',
+      description   = 'easier csv reading/writing from numpy',
+      license       = 'MIT',
+      keywords      = 'numpy csv',
+      author        = 'Brent Pedersen',
+      author_email  = 'bpederse@gmail.com',
+      url   = '',
+      packages      = [''],
+      requires = ['numpy'],
+      test_suite = 'tests',
+      classifiers   = [
+        'Development Status :: 4 - Beta'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering'
+        ],
+)
---
+++ npytxt/tests.py
@@ -0,0 +1,54 @@
+import numpy as np
+from npytxt import savetxt, loadtxt
+from cStringIO import StringIO
+
+import unittest
+import os
+
+class NpyTxtTester(unittest.TestCase):
+    def setUp(self):
+        d = StringIO("M 21 72.1\nF 35 58.33")
+        self.a =  np.loadtxt(d, dtype={'names' : ('gender', 'age', 'weight')
+                       , 'formats': ('S1', 'i4', 'f8')})
+    
+        savetxt(self.a, "g.txt")
+
+    def test_save(self):
+        self.assert_(os.path.exists("g.txt"))
+        self.assert_(os.path.exists("g.txt.nkl"))
+
+        txt = open("g.txt")
+        self.assertEqual(len(txt.readlines()), 3)
+
+    def test_load(self):
+    
+        b = loadtxt("g.txt")
+        self.assertEqual(self.a.dtype, b.dtype, (self.a.dtype, b.dtype))
+        self.assertEqual(self.a[1], b[1])
+
+        # can extract by column
+        self.assertEqual(b[b['gender'] == 'M'].shape[0], 1)
+
+
+    def test_names(self):
+        # set the names manually, and it will figure out the format string. 
+        # from the dtype of the array.
+        savetxt(self.a, "g.txt", names=("a", "b", "c"))
+        c = loadtxt("g.txt")
+        self.assertEqual(["a", "b", "c"], sorted(c.dtype.fields.keys()))
+
+        self.assertEqual(self.a[1]['age'], c[1]['b'])
+        self.assertEqual(c[0]['c'], 72.1, c[0])
+
+
+    def tearDown(self):
+        os.unlink("g.txt")
+        os.unlink("g.txt.nkl")
+        pass
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
