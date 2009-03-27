5efd7b14b6ce23c066574a6465e519ce67d49c70
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-03-27 03:35:20 +0000

    add better stuff for nodes, but current failing test shows a big prob with the parsing. need to actually use id, parent stuff. doi.

--- biostuff/biostuff/gff_reader.py
+++ biostuff/biostuff/gff_reader.py
@@ -2,39 +2,56 @@ import mmap
 import re
 import sys
 
-def get_non_comment_line(fh):
+def _get_non_comment_line(fh):
     while True:
         line = fh.readline()
         if line[0] == '#': continue
         return line
 
-def get_lines_until_next(fh, feature):
-    lines = [get_non_comment_line(fh)]
+def _get_lines_until_next(fh, feature):
+    lines = [_get_non_comment_line(fh)]
     while not feature in lines[-1]:
         if not lines[-1]: return lines # end of file
-        lines.append(get_non_comment_line(fh))
+        lines.append(_get_non_comment_line(fh))
     return lines
 
 class GFFNode(object):
-    def __init__(self):
-        pass
+    def __init__(self, node_list, top_level_feature='gene'):
+        self.start = min(n.start for n in node_list)
+        self.stop = max(n.stop for n in node_list)
+        self.end = self.stop
+        assert node_list[0].type == top_level_feature, node_list[0]
+        self.parent = node_list[0]
+        self.nodes = node_list[1:]
+
+        assert self.parent.start == self.start, (self.start, self.parent.start)
+        assert self.parent.end == self.end, (self.end, self.parent.end)
+    
+    def __repr__(self):
+        return "GFFNode(%s: %i .. %i, %i sub-nodes)" % \
+                   (self.parent.attrs.get('ID', ''), self.start, self.end,
+                    len(self.nodes))
+
 
     @classmethod
     def yield_nodes(cls, fh, top_level_feature='gene'):
         gene = top_level_feature
+        close = False
         if not hasattr(fh, 'read'):
+            close = True
             fh = open(fh, 'r')
 
-        next_gene_line = get_non_comment_line(fh)
+        next_gene_line = _get_non_comment_line(fh)
         while True:
             assert gene in next_gene_line
-            block = get_lines_until_next(fh, gene)
+            block = _get_lines_until_next(fh, gene)
             block.insert(0, next_gene_line)
             next_gene_line = block.pop()
 
-            yield [GFFLine(l) for l in block]
+            yield GFFNode([GFFLine(l) for l in block], top_level_feature=gene)
 
 
+        if close: fh.close()
 
 
 class GFFLine(object):
@@ -59,6 +76,19 @@ class GFFLine(object):
                 attrs[pair[0]] = pair[1]
         if attrs == {}: attrs["ID"] = line[8]
         return attrs
+
+    @classmethod
+    def yield_lines(cls, fh):
+        close = False
+        if not hasattr(fh, 'read'):
+            fh = open(fh, 'r')
+            close = True
+        line = _get_non_comment_line(fh)
+        while line:
+            yield GFFLine(line)
+            line = _get_non_comment_line(fh)
+    
+        if close: fh.close()
 
     def __repr__(self):
         return "GFFLine(%s %s:%i .. %i)" % (self.seqid,
--- biostuff/tests/test_gff.py
+++ biostuff/tests/test_gff.py
@@ -1,24 +1,32 @@
 from biostuff.gff_reader import GFFLine, GFFNode
 import sys
 
+GFF = "tests/data/th.gff" 
 
 def test_gff_node():
-    f = "tests/data/th.gff" 
 
-    node = GFFNode.yield_nodes(f)
-    print >>sys.stderr, node.next(), "\n"
-    print >>sys.stderr, node.next(), "\n"
-    print >>sys.stderr, node.next(), "\n"
-    print >>sys.stderr, node.next(), "\n"
-    print >>sys.stderr, node.next(), "\n"
+    node_gen = GFFNode.yield_nodes(GFF)
+    n = node_gen.next()
+    assert len(n.nodes) == 0
+
+    n = node_gen.next()
+    assert n.parent.start == 3631
+    assert len(n.nodes) == 12
+
+    n = node_gen.next()
+    assert n.parent.attrs["ID"] == "thaliana_1_6160_6269", n.parent
+
+    n = node_gen.next()
+    assert n.parent.end == 8737, n.parent
+
+def test_gff_all():
+    for f in GFFNode.yield_nodes(GFF): pass
+
+def test_gff_line():
+    line_gen = GFFNode.yield_lines(GFF)
+
+    print >>sys.stderr, line_gen
 
 
-    #yield check_type, blasts, ('eval', 'score', 'pctid'), float
-    #yield check_type, blasts, ('query', 'subject'), str
-    
-
-def check_type(blasts, attrs, klass):
-    for b in blasts:
-        for attr in attrs:
-            assert isinstance(getattr(b, attr), klass)
-
+def test_gff_line_all():
+    for f in GFFNode.yield_lines(GFF): pass
