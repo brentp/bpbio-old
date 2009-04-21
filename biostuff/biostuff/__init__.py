e4249e90a8e914b6156b1eea6b92204dd97b1097
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-04-21 21:27:19 +0000

    add the c file, start the command line scripts template.

--- biostuff/biostuff/__init__.py
+++ biostuff/biostuff/__init__.py
@@ -1,8 +1,23 @@
-from blast_line import BlastLine as PyBlastLine
+#from blast_line import BlastLine as PyBlastLine
 from cblastline import BlastLine
 from gff_reader import GFFNode, GFFLine
 
 
 def main():
+    progs = ['cblastline']
+    if len(sys.argv) == 1 or not sys.argv[1] in progs:
+        print """Usage:
+    $ biostuff prog [opts]
+or 
+    $ biostuff prog -h
+
+where current prog's are: %s
+        """ % "\n".join(progs)
+    else:
+        prog = sys.argv.pop(1)
+        mod = __import__(prog)
+        mod.main()
+
+if __name__ == "__main__":
     import sys
-    print sys.argv
+    main()
