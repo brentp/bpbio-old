c746fd42ea845c5fcbc70f75e9a5fea4bb6b103d
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-04-22 00:02:56 +0000

    gap extension score separate from gap init score

--- trunk/nwalign/nwalign.pyx
+++ trunk/nwalign/nwalign.pyx
@@ -4,6 +4,7 @@ cimport numpy as np
 
 cimport cython
 import sys
+import os.path
 
 
 cdef extern from "stdlib.h":
@@ -23,10 +24,11 @@ cdef extern from "Python.h":
 
 ctypedef np.int_t DTYPE_INT
 ctypedef np.uint_t DTYPE_UINT
+ctypedef np.int8_t DTYPE_BOOL
 
 cdef size_t UP = 0, LEFT = 1, DIAG = 2, NONE = 3
 
-MATRIX = {}
+MATRIX = ['BLOSUM62']
 
 cdef inline int imax2(int a, int b):
     if a >= b: return a
@@ -39,7 +41,15 @@ def read_matrix(path):
     cdef size_t ai = 0, i
     cdef int v
 
-    if path in MATRIX: return MATRIX[path]
+    #if os.path.basename(path) in MATRIX:
+    #    import cPickle
+    #    op = os.path
+    #    fpath = op.join(op.dirname(op.abspath(__file__)), 'data', path + '.pkl')
+    #    fh = open(fpath, 'rb')
+    #    a = cPickle.load(fh)
+    #    h = cPickle.load(fh)
+    #    fh.close()
+    #    return h, a
 
     fh = open(path)
     headers = None
@@ -71,7 +81,7 @@ cdef inline size_t strpos(char *tstr, char check):
 
 
 @cython.boundscheck(False)
-cpdef global_align(object _seqj, object _seqi, int gap=-1, int match=1, int mismatch=-1, object matrix=None):
+cpdef global_align(object _seqj, object _seqi, int gap=-1, int match=1, int mismatch=-1, int gap_init=-1, object matrix=None):
     """
     perform a global sequence alignment (needleman-wunsch) on seq and and 2. using
     the matrix for nucleotide transition from matrix if available.
@@ -94,11 +104,14 @@ cpdef global_align(object _seqj, object _seqi, int gap=-1, int match=1, int mism
     cdef char ci, cj
     cdef size_t ii, jj
     cdef PyObject *ai, *aj
+    cdef int zero=0, one=1
 
 
+    cdef np.ndarray[DTYPE_BOOL, ndim=2] agap = np.zeros((max_i + 1, max_j + 1), dtype=np.int8)
     cdef np.ndarray[DTYPE_INT, ndim=2] score = np.zeros((max_i + 1, max_j + 1), dtype=np.int)
     cdef np.ndarray[DTYPE_UINT, ndim=2] pointer = np.zeros((max_i + 1, max_j + 1), dtype=np.uint)
     cdef np.ndarray[DTYPE_INT, ndim=2] amatrix
+
 
 
     if matrix is not None:
@@ -113,7 +126,11 @@ cpdef global_align(object _seqj, object _seqi, int gap=-1, int match=1, int mism
 
     score[<size_t>0, <size_t>1:] = gap * np.arange(max_j, dtype=np.int)
     score[<size_t>1:, <size_t>0] = gap * np.arange(max_i, dtype=np.int)
-    
+
+    agap[<size_t>0, <size_t>1:] = one
+    agap[<size_t>1:, <size_t>0] = one
+    agap[<size_t>0, <size_t>0] = zero
+
     for i in range(1, max_i + 1):
         ci = seqi[i - 1]
         for j in range(1, max_j + 1):
@@ -124,28 +141,36 @@ cpdef global_align(object _seqj, object _seqi, int gap=-1, int match=1, int mism
             else:
                 ii = strpos(sheader, ci)
                 jj = strpos(sheader, cj)
-                if ii == -1:
-                    py_ci = <object>PyString_FromStringAndSize(&ci, <size_t>1)
-                    raise Exception(py_ci + "from: " + seqi  + " not in scoring matrix")
-                if jj == -1:
-                    py_cj = <object>PyString_FromStringAndSize(&cj, <size_t>1)
-                    print py_cj, "not in string:", seqj
-                    raise Exception(py_cj + "from: " + seqj + " not in scoring matrix")
-                tscore = amatrix[ii, jj]
+                if ii == -1 or jj == -1:
+                    if ii == -1:
+                        py_ci = <object>PyString_FromStringAndSize(&ci, <size_t>1)
+                        #raise Exception(py_ci + "from: " + seqi  + " not in scoring matrix")
+                        sys.stderr.write("'" + py_ci + "' from: " + seqi  + " not in scoring matrix\n")
+                    if jj == -1:
+                        py_cj = <object>PyString_FromStringAndSize(&cj, <size_t>1)
+                        #raise Exception(py_cj + "from: " + seqj + " not in scoring matrix")
+                        sys.stderr.write("'" + py_cj + "' from: " + seqj + " not in scoring matrix\n")
+                    sys.stderr.write("using score of score of 0\n")
+                    tscore = 0
+                else:
+                    tscore = amatrix[ii, jj]
+
                 diag_score = score[i - 1, j - 1] + tscore
 
-            up_score   = score[i - 1, j] + gap
-            left_score = score[i, j - 1] + gap
+            up_score = score[i - 1, j] + (gap if agap[i - 1, j] == one else gap_init)
+            left_score   = score[i, j - 1] + (gap if agap[i, j - 1] == one else gap_init)
             
             if diag_score >= up_score:
                 if diag_score >= left_score:
                     score[i, j] = diag_score
                     pointer[i, j] = DIAG
+                    agap[i, j] = zero
                 else:
                     score[i, j] = left_score
                     pointer[i, j] = LEFT
-
+                    agap[i, j] = one
             else:
+                agap[i, j] = one
                 if up_score > left_score:
                     score[i, j]  = up_score
                     pointer[i, j] = UP
@@ -190,7 +215,8 @@ def main():
     parser = optparse.OptionParser(usage="""
     %prog [options] seq1 seq2 
     """)
-    parser.add_option("--gap", dest="gap", help="gap penalty (must be integer < 0)", type="int", default=-1)
+    parser.add_option("--gap", dest="gap", help="gap extend penalty (must be integer <= 0)", type="int", default=-1)
+    parser.add_option("--gap_init", dest="gap_init", help="gap start penalty (must be integer <= 0)", type="int", default=-1)
     parser.add_option("--match", dest="match", help="match score (must be integer > 0)", type="int", default=1)
     parser.add_option("--mismatch", dest="mismatch", help="gap penalty (must be integer < 0)", type="int", default=-1)
     parser.add_option("--matrix", dest="matrix", help="scoring matrix in ncbi/data/ format,\
@@ -203,5 +229,5 @@ def main():
 
     if len(args) != 2:
         sys.exit(parser.print_help())
-    print "\n".join(global_align(args[0], args[1], options.gap, options.match, options.mismatch, options.matrix))
+    print "\n".join(global_align(args[0], args[1], options.gap, options.match, options.mismatch, options.gap_init, options.matrix))
         
