6b3da7ee34b24082507b1b0deaab86e370c1d09f
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-05-27 21:01:18 +0000

    update repr on FastaGz class more description in setup.py

--- trunk/pyfasta/setup.py
+++ trunk/pyfasta/setup.py
@@ -6,15 +6,17 @@ version = '0.1'
 
 setup(name='pyfasta',
       version=version,
-      description="a convenience class for dealing with fasta files in python",
+      description="pythonic access to fasta sequence files",
+      url="http://code.google.com/p/bpbio/",
+      long_description=open('README.txt').read(),
       classifiers=["Topic :: Scientific/Engineering :: Bio-Informatics"],
       keywords='bioinformatics blast',
       author='brentp',
       author_email='bpederse@gmail.com',
-      url='',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
+      test_suite='nose.collector',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
