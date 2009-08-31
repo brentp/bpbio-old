d8a985b5e6ffc61b7136510a57641e8e23d2122b
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-08-31 22:30:49 +0000

    add labels for x position

--- trunk/mpldraw/genecanvas.py
+++ trunk/mpldraw/genecanvas.py
@@ -11,11 +11,14 @@ class Gene(FancyArrow):
         if strand in ('-1', '-', -1):
             x = stop
             dx = start - stop
-            y = -0.25
+            y = -0.22
         else:
             x = start
             dx = stop - start
-            y = 0.25
+            y = 0.22
+
+        # move it down a bit since text goes above.
+        y -= 0.05
 
         head_length = min(10, max(20, (stop - start)/ 4))
         self._textx = start
@@ -31,12 +34,16 @@ class SimpleFigure(Figure):
         figsize = (figsize[0] / dpi, figsize[1] / dpi)
         Figure.__init__(self, figsize=figsize, dpi=dpi)
         self.patch.set_alpha(0.0)
+
         self.canvas = FigureCanvas(self)
-        w, h = self.get_figwidth(), self.get_figheight()
-        ax = self.add_axes([0, 0, 1, 1], frameon=False, autoscale_on=False, aspect='auto')
-        ax.set_xticks([])
+        ax = self.add_axes([0, 0.22, 1.0, .78], autoscale_on=False, aspect='auto', alpha=0.0)
+
+        for loc, sp in ax.spines.iteritems():
+            if loc != 'bottom': sp.set_color('none')
+            #else: sp.set_position('center')
+
         ax.set_yticks([])
-        ax.set_ylim(-1, 1)
+        ax.patch.set_alpha(0.0)
         self.ax = ax
 
 
@@ -48,17 +55,19 @@ class SimpleFigure(Figure):
     def add_patch(self, patch):
         self.ax.add_patch(patch)
         if hasattr(patch, 'text'):
-            self.ax.text(patch._textx, patch._texty, patch.text, va='top', ha='left')
+            self.ax.text(patch._textx, patch._texty, patch.text, va='top', ha='left', fontsize=10)
 
     def save(self, filename):
+        for xt in self.ax.get_xticklabels():
+            xt.set_fontsize(8)
         self.canvas.print_figure(filename, dpi=self.dpi)
     savefig = save
 
 
 class GeneFigure(SimpleFigure):
-    def __init__(self, figsize=(512, 256), dpi=96):
+    def __init__(self, figsize=(512, 96), dpi=96):
         SimpleFigure.__init__(self, figsize, dpi)
-        self.ax.set_ylim(-1.5, 1.5)
+        self.ax.set_ylim(-0.5, 0.5)
 
     def set_xlim(self, xmin, xmax):
         self.ax.set_xlim(xmin, xmax)
@@ -80,5 +89,8 @@ if __name__ == "__main__":
     g3 = Gene("At2g25640", 140, 290, -1)
     gf.add_patch(g3)
 
-    gf.set_xlim(0, 300)
+    g4 = Gene("At2g25640", 1400, 1590, 1)
+    gf.add_patch(g4)
+
+    gf.set_xlim(0, 1600)
     gf.save('t.png')
