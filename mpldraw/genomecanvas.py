d9b59822e444803cd84a9d8c1714245568001b02
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-09-04 20:41:57 +0000

    add a nicer genome drawing thingy

--- mpldraw/genomecanvas.py
+++ mpldraw/genomecanvas.py
@@ -4,45 +4,42 @@ from matplotlib.figure import Figure
 from cStringIO import StringIO
 from matplotlib.ticker import Formatter, LinearLocator
 
+BOTTOM_AXIS, MIDDLE_AXIS, TOP_AXIS = range(3)
+
 class BasePairTickFormatter(Formatter):
     def __call__(self, x, pos=None):
         fmts = ((1e9, 'G'), (1e6, 'M'), (1e3, 'K'))
         for div, fmt in fmts:
             if x * 2 > div:
                 x = "%.3f" % (float(x) / div)
-                x = x.rstrip('0').rstrip('.') + fmt
-                break
-        else:
-            x = str(int(x))
-        return x
+                return x.rstrip('0').rstrip('.') + fmt
+        return str(int(x))
 
 class BasePairLocator(LinearLocator):
     pass
-    
+
+ 
 
 def xy_dxy(start, stop, strand):
         if strand in ('-1', '-', -1):
             x = stop
             dx = start - stop
-            y = -0.35
         else:
             x = start
             dx = stop - start
-            y = 0.35 if strand else 0.0
-        return x, y, dx
+        # set y to 0.25 as the lables go above.
+        return x, 0.25, dx
 
 class Gene(FancyArrow):
-    def __init__(self, text, start, stop, strand=None, width=0.07, ec='none', **kwargs):
+    def __init__(self, text, start, stop, strand=None, width=0.14, ec='none', **kwargs):
         assert start <= stop
         self.text = text
 
         x, y, dx = xy_dxy(start, stop, strand)
-        # move it down a bit since text goes above.
-        y -= 0.05
         head_length = self.get_head_length(start, stop, strand, kwargs)
 
         self._textx = start
-        self._texty = y + (0.5 * width) + .16
+        self._texty = y + (2 * width) + .03
         if not ec in ('none', None) and not kwargs.get('fc'):
             kwargs['fc'] = ec
         FancyArrow.__init__(self, x, y, dx, 0, head_width=width * 1.35, head_length=head_length, width=width, length_includes_head=True, ec=ec, **kwargs)
@@ -59,19 +56,20 @@ class Gene(FancyArrow):
         return head_length
 
 class Block(Rectangle):
-    def __init__(self, text, start, stop, strand=None, width=0.3, ec='none', **kwargs):
-        x, y, dx = xy_dxy(start, stop, strand)
+    def __init__(self, text, start, stop, strand=None, width=0.5, ec='none', y=None, **kwargs):
+        x, _, dx = xy_dxy(start, stop, strand)
+        if y is None:
+            y = 0.5
         self._textx = start + abs(dx)/ 20.
         self._texty = y 
         self.text = text
         w = width
-        y -= 0.05
         if not ec in ('none', None) and not kwargs.get('fc'):
             kwargs['fc'] = ec
         Rectangle.__init__(self, (x, y - w / 2.0), width=dx, height=w, ec=ec, **kwargs)
 
 class HSP(Block):
-    def __init__(self, start, stop, strand=None, width=0.09, fc='#cacaca', ec='#777777', **kwargs):
+    def __init__(self, start, stop, strand=None, width=0.18, fc='#cacaca', ec='#777777', **kwargs):
         Block.__init__(self, None, start, stop, strand, width=width, fc=fc, ec=ec, *kwargs)
     
 
@@ -79,46 +77,76 @@ class CDS(Block):
     def __init__(self, start, stop, strand=None, **kwargs):
         fc = kwargs.get('fc', 'red')
         kwargs['fc'] = fc
-        kwargs['width'] = kwargs.get('width', 0.09)
+        kwargs['width'] = kwargs.get('width', 0.18)
         kwargs['ec'] = 'black'
 
         Block.__init__(self, None, start, stop, strand, **kwargs)
 
 
-class SimpleFigure(Figure):
-    def __init__(self, figsize=(512, 256), dpi=96):
+class ThreeAxFigure(Figure):
+    def __init__(self, figsize=(768, 196), dpi=96):
         dpi = float(dpi)
         figsize = (figsize[0] / dpi, figsize[1] / dpi)
         Figure.__init__(self, figsize=figsize, dpi=dpi)
         self.patch.set_alpha(0.0)
 
         self.canvas = FigureCanvas(self)
-        ax = self.add_axes([0, 0.22, 1.0, .78], autoscale_on=False, aspect='auto', alpha=0.0)
+        self.setup_axes()
 
-        for loc, sp in ax.spines.iteritems():
-            if loc != 'bottom': sp.set_color('none')
-            #else: sp.set_position('center')
+    def setup_axes(self):
+        from mpl_toolkits.axes_grid import axes_size as Size
+        from mpl_toolkits.axes_grid import Divider
 
-        ax.set_yticks([])
-        ax.patch.set_alpha(0.0)
-        self.ax = ax
+        rect = (0, 0.05, 1, 1) 
+        axes = [self.add_axes(rect, autoscale_on=False, aspect='auto', alpha=1.0, label=str(i)) for i in range(3)]
+        
+
+        hori = [Size.AxesX(axes[0])]
+        vert = [Size.Scaled(0.20), Size.Scaled(0.60), Size.Scaled(0.20)]
+
+        d = Divider(self, rect, hori, vert) 
+
+        for i, ax in enumerate(axes):
+            ax.set_axes_locator(d.new_locator(nx=0, ny=i))
+
+        self.ax = axes
+        self._setup_axes()
 
 
-    def set_bbox(self, bbox):
-        self.ax.set_xlim(bbox[0], bbox[2])
-        self.ax.set_ylim(bbox[1], bbox[3])
+    def _setup_axes(self):
+        """ set the ranges and spines"""
+
+        for ax in self.ax:
+            for loc, sp in ax.spines.iteritems():
+                if loc != 'bottom': sp.set_color('none')
+        self.ax[BOTTOM_AXIS].xaxis.set_major_formatter(BasePairTickFormatter())
+
+        for ax in self.ax:
+            ax.set_yticks([])
+            ax.patch.set_alpha(0.0)
+
+        for ax in (MIDDLE_AXIS, TOP_AXIS):
+            self.ax[ax].set_xticks([])
+
+        for ax in (BOTTOM_AXIS, TOP_AXIS):
+            self.ax[ax].set_ylim(0, 1)
+
+    def set_xlim(self, xmin, xmax):
+        for ax in self.ax:
+            ax.set_xlim(xmin, xmax)
 
 
-    def add_patch(self, patch):
-        self.ax.add_patch(patch)
+    def add_patch(self, patch, axis=None):
+        self.axes[axis].add_patch(patch)
         if hasattr(patch, 'text'):
             if not patch.text is None:
-                self.ax.text(patch._textx, patch._texty, patch.text, va='top', ha='left', fontsize=8.5)
+                self.axes[axis].text(patch._textx, patch._texty, patch.text, va='top', ha='left', fontsize=8.5)
 
-    def save(self, filename=None):
+    def save(self, filename=None, fontsize=8):
 
-        for xt in self.ax.get_xticklabels():
-            xt.set_fontsize(8)
+        for xt in self.axes[BOTTOM_AXIS].get_xticklabels():
+            xt.set_fontsize(fontsize)
+
         if filename is None:
             s = StringIO()
             self.canvas.print_figure(s, dpi=self.dpi)
@@ -130,47 +158,41 @@ class SimpleFigure(Figure):
     savefig = save
 
 
-class GeneFigure(SimpleFigure):
-    def __init__(self, figsize=(768, 96), dpi=96):
-        SimpleFigure.__init__(self, figsize, dpi)
-        self.ax.set_ylim(-0.5, 0.5)
-        self.ax.xaxis.set_major_formatter(BasePairTickFormatter())
-        #self.ax.xaxis.set_major_locator(BasePairLocator())
-        #self.ax.grid()
-
-    def set_xlim(self, xmin, xmax):
-        self.ax.set_xlim(xmin, xmax)
-
-
+class GenomeFigure(ThreeAxFigure):
+    pass
 
 
 if __name__ == "__main__":
 
-    gf = GeneFigure()
+    gf = GenomeFigure()
 
     g1 = Gene("G1", 0, 100, 1)
-    gf.add_patch(g1)
+    gf.add_patch(g1, TOP_AXIS)
 
     g2 = Gene("G@", 10, 130, -1)
-    gf.add_patch(g2)
+    gf.add_patch(g2, BOTTOM_AXIS)
 
     cds = CDS(60, 90, -1)
-    gf.add_patch(cds)
+    gf.add_patch(cds, BOTTOM_AXIS)
 
 
     g3 = Gene("At2g25640", 140, 290, -1)
-    gf.add_patch(g3)
+    gf.add_patch(g3, BOTTOM_AXIS)
 
     g4 = Gene("At2g25640", 1400, 1590, 1)
-    gf.add_patch(g4)
+    gf.add_patch(g4, TOP_AXIS)
 
-    nostrand = Block("someblock", 1000, 1200, fc='red', ec='black')
-    gf.add_patch(nostrand)
+    nostrand = Block("someblock", 1000, 1200, fc='red', ec='black', y=1200)
+    gf.add_patch(nostrand, MIDDLE_AXIS)
 
-    gf.add_patch(HSP(1460, 1503))
+    gf.add_patch(HSP(1460, 1503), MIDDLE_AXIS)
+    print sorted(dir(gf.ax[MIDDLE_AXIS].yaxis))#.set_autoscale_on(True)
+    gf.ax[MIDDLE_AXIS].yaxis.set_scale('auto')
+    print gf.ax[MIDDLE_AXIS].get_ylim()
 
     gf.set_xlim(0, 1600)
     gf.save('/var/www/t/t.png')
+
  
     # the buffer
     print len(gf.save())
