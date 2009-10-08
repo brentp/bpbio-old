568a082686f3dbca4408192e81a1d2008f9a0b89
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-10-08 17:26:11 +0000

    better docs, prep for pypi

--- trunk/nwalign/setup.py
+++ trunk/nwalign/setup.py
@@ -1,36 +1,46 @@
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
 
 version = '0.1'
 import numpy
 np_include = numpy.get_include()
+import nwalign
 
 setup(name='nwalign',
       version=version,
-      description="",
-      long_description="""\
-""",
+      description="Needleman-Wunsch global sequence alignment",
+      long_description=nwalign.__doc__,
       ext_modules=[ Extension("nwalign",
-                      sources=["nwalign.pyx"],
-                      include_dirs=[np_include, ","])],
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='bio',
+                              #sources=["nwalign.pyx"],
+                      sources=["nwalign.c"],
+                      include_dirs=[np_include, "."])],
+      keywords='bioinformatics alignment needleman-wunsch',
       author='brentp',
       author_email='bpederse@gmail.com',
       url='',
       license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']), # + ['.'],
+      packages=find_packages(exclude=[]), # + ['.'],
       test_suite='nose.collector',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          'numpy', 'cython'
       ],
       entry_points= {
           # -*- Entry points: -*-
           'console_scripts': ['nwalign = nwalign:main']
           },
-      #package_data={'': ['data/*.pkl']},
-      #data_files=['data/*.pkl']
-      )
+    classifiers   = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Text Processing'
+        ],
+
+)
