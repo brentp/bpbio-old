4c9ca1587cfbd8f842275cda7d6cc88fdafa333f
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2009-12-21 05:41:16 +0000

    allow ignoring local duplicates on same chromosomes. alow plotting points (much faster).

--- scripts/dagchainer/dagtools/plot_dag.py
+++ scripts/dagchainer/dagtools/plot_dag.py
@@ -1,5 +1,6 @@
 import sys
 from cdagline import DagLine
+import numpy as np
 from itertools import cycle
 
 from matplotlib import pyplot as plt
@@ -9,24 +10,35 @@ ax = f.add_subplot(1, 1, 1)
 reverse = False
 xmax = 0
 ymax = 0
+LINES = False
 
-colors = cycle('rgbcky')
+colors = cycle('rgbcmyk')
 
-
+c = 'y'
+pts = []
 for line in open(sys.argv[1]):
     if line[0] == '#': 
-        c = colors.next()
-        reverse = 'reverse' in line
+        if sys.argv[2] in line and sys.argv[3] in line:
+            c = colors.next()
         continue
     dag = DagLine(line)
 
     if dag.a_seqid != sys.argv[2]: continue
     if dag.b_seqid != sys.argv[3]: continue
 
-    ax.plot([dag.a_start, dag.a_end], 
-            [dag.b_start, dag.b_end], c=c)
+    if LINES:
+        ax.plot([dag.a_start, dag.a_end], 
+                [dag.b_start, dag.b_end], c=c)
+    else:
+        pts.append((dag.a_start, dag.b_start, c))
+
     if dag.a_end > xmax: xmax = dag.a_end
     if dag.b_end > ymax: ymax = dag.b_end
+
+if not LINES:
+    pts = np.array(pts, dtype=[('x', int), ('y', int), ('c', 'S1')])
+    ax.scatter(pts['x'], pts['y'], edgecolor='none', c=pts['c'], s=2)
+
 
 ax.set_xlim(0, xmax)
 ax.set_ylim(0, ymax)
