0a9240094487b7f65b362de8c2f0357607d7d188
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2010-01-04 22:14:11 +0000

    add an example to the README. clean up the merge genes stuff so that small, close diagonals are merged into a single dag.

--- trunk/scripts/dagchainer/README.rst
+++ trunk/scripts/dagchainer/README.rst
@@ -12,3 +12,21 @@ Changes
 
 4. the Makefile was update to include compile flags that make the resulting
    executable up to 2 times as fast.
+
+
+Example
+-------
+
+1. build the dagchainer executable and the python libs ::
+
+  make
+
+2. an example shell script using the atat.dag from arabidopsis :
+
+    IN=atat.dag
+    OUT=a.out
+    python dagtools/evalue_adjust.py -c 4 $IN | python dag_chainer.py -g 40000 -D 160000 -A 4 --merge $OUT -
+    python dagtools/plot_dag.py -q athaliana_3 -s athaliana_5 -p dotplot.png -d ${OUT}.meta
+
+
+3. and view the output in dotplot.png
--- trunk/scripts/dagchainer/dag_chainer.py
+++ trunk/scripts/dagchainer/dag_chainer.py
@@ -28,16 +28,17 @@ def scoringF(evalue, constant_match=CONSTANT_MATCH_SCORE, max_match=MAX_MATCH_SC
 
 def get_dag_line(fh):
     line = fh.readline()
-    if not line: return None
+    if not line: return None, None
     if line[0] == "#": 
-        get_dag_line.header = parse_pyheader(line, True)
+        get_dag_line.header = parse_pyheader(line, asstring=True)
         line = fh.readline()
-    return DagLine(line)
+    return DagLine(line), get_dag_line.header
 # `get_dag_line.header` is sort of a hack to save the last seen header.
 # so we know every dagline that follows belong to this group.
 get_dag_line.header = None
 
-JS="^"
+JS="^" # magic separator. (J)oin (S)tring
+
 def parse_pyheader(header, asstring=False):
     cols = ('id', 'dagscore', 'a_seqid', 'b_seqid', 'dir', 'ngenes')
     #1  17397.0 athaliana_1 athaliana_1 f   432
@@ -60,7 +61,7 @@ def get_meta_gene(fh, header=[None]):
         d = DagLine(line)
         genes.append(d)
         line = fh.readline()
-    if len(genes) == 0: return None
+    if len(genes) == 0: return None, None
     l = header[0]
     header_string = parse_pyheader(header[0], asstring=True)
     # save the next header.
@@ -85,26 +86,24 @@ def get_meta_gene(fh, header=[None]):
          'a_end': a_end, 
          'b_end': b_end, 
          'evalue': 1e-250}
-    return DagLine.from_dict(d)
+    return DagLine.from_dict(d), header_string
     
 
-def parse_file(dag_file, evalue_cutoff, ignore_dist, merge_genes=False, counts=None):
+def parse_file(dag_file, evalue_cutoff, ignore_dist, merge_genes=False):
+    """ if dag_file is "-", then the stuff is read from stdin. """
 
     accn_info = {}
     matches = {}
-    fh = open(dag_file)
+    fh = open(dag_file) if dag_file != "-" else sys.stdin
     dag = True
     while dag:
         if merge_genes:
-            dag = get_meta_gene(fh)
+            dag, dag_header = get_meta_gene(fh)
             if dag is None: break
 
         else:
-            dag = get_dag_line(fh)
+            dag, dag_header = get_dag_line(fh)
             if dag is None: break
-            if counts is not None:
-                counts[dag.a_accn] += 1
-                counts[dag.b_accn] += 1
 
             if dag.evalue >= evalue_cutoff: continue
             if dag.a_seqid == dag.b_seqid:
@@ -146,16 +145,11 @@ def parse_file(dag_file, evalue_cutoff, ignore_dist, merge_genes=False, counts=N
             if dag.evalue < these_matches[accn_key]['evalue']: these_matches[accn_key]['evalue'] = dag.evalue
         else:
             these_matches[accn_key] = {'A': a_feat, 'B': b_feat, 'evalue': dag.evalue}
-            # if it's running through again to merge meta genes, we want to
-            # know which diagonal each match is from. counts indicates
-            # meta-genes.
-            if counts is not None:
-                 these_matches[accn_key]['diag_str'] = get_dag_line.header
+            these_matches[accn_key]['diag_str'] = dag_header
 
+    get_dag_line.header = None
     return matches
 
-# TODO: do a filter on the final output by count of repeats and % of a dag group that
-# is made up of repeats.
 
 def parse_cheader(header):
     """ dagchainer.cpp sends a header line: ">Alignment #%d  score = %.1f\n" 
@@ -230,9 +224,11 @@ def print_alignment(dir, diag_num, dag_score, group, opts, out):
                      B['seqid'], B['accn'], B['start'], B['end'],
                      pair_dict['pair']['evalue'], pair_dict['dag_score'])
 
-def iterate_matches(all_matches, opts, save_meta=False, out=sys.stdout): 
+def run_and_print(all_matches, opts, out=sys.stdout): 
     filename = "-" # tells dagchainer to read from stdin.
-    merge_genes = True if opts.merge else None
+    # if out is False, it means we dont want to print, and so we 
+    # dont print.
+    print_genes = bool(out)
     meta_ids = []
 
     for (a_seqid, b_seqid), matches in sorted(all_matches.iteritems()):
@@ -246,7 +242,7 @@ def iterate_matches(all_matches, opts, save_meta=False, out=sys.stdout):
         pr.start()
 
         for dag_num, dag_score, group in parent_connf.recv():
-            if merge_genes is None:
+            if print_genes:
                 print_alignment('f', dag_num, dag_score, group, opts, out)
             else:
                 # for merged meta we just keep the direction and the 'accn' where
@@ -259,7 +255,7 @@ def iterate_matches(all_matches, opts, save_meta=False, out=sys.stdout):
                                  len(group)))
 
         for dag_num, dag_score, group in parent_connr.recv():
-            if merge_genes is None:
+            if print_genes:
                 print_alignment('r', dag_num, dag_score, group, opts, out)
             else:
                 meta_ids.append(('r', a_seqid, b_seqid,
@@ -281,13 +277,10 @@ def iterate_matches(all_matches, opts, save_meta=False, out=sys.stdout):
 # meta
 [('f', [('a8^252.0^athaliana_1^athaliana_1^f^18', 'b8^252.0^athaliana_1^athaliana_1^f^18'), ('a4^100.0^athaliana_1^athaliana_1^r^5', 'b4^100.0^athaliana_1^athaliana_1^r^5')]), ('f', [('a7^486.0^athaliana_1^athaliana_1^f^43', 'b7^486.0^athaliana_1^athaliana_1^f^43'), ('a1^6750.0^athaliana_1^athaliana_1^f^414', 'b1^6750.0^athaliana_1^athaliana_1^f^414')]), ('f', [('a11^154.0^athaliana_1^athaliana_1^
 """
-def merge_meta(meta, opts, max_count=10):
+def merge_meta(meta, all_matches, opts, out):
     """ merge the meta genes with the 
     original dag data sent in"""
     #cols = ('id', 'dagscore', 'a_seqid', 'b_seqid', 'dir', 'ngenes')
-    counts = collections.defaultdict(int)
-    all_matches = parse_file(opts.dag, opts.evalue, opts.ignore_dist, counts=counts)
-    fhmeta = sys.stdout
     by_diag = matches_by_diag_id(all_matches)
     # wnat the longest first. then we remove shorter ones that are completely contained
     # in the larger ones.
@@ -301,22 +294,22 @@ def merge_meta(meta, opts, max_count=10):
         # TODO: need to sort this out better. can meta-diags with opposite directions
         # be merged??? when?
         for diag_str in diag_str_list:
-            these_dags = []
             if not diag_str in by_diag: continue
-            for d in by_diag[diag_str]:
-                these_dags.append(d)  
+            these_dags = by_diag[diag_str]
+            #for d in by_diag[diag_str]:
+            #    these_dags.append(d)  
             unseen_dags = [t for t in these_dags if not (t.a_accn, t.b_accn) in seen]
             for ud in unseen_dags: seen[(ud.a_accn, ud.b_accn)] = None
-            #if len(these_dags) >= opts.min_aligned_pairs:
-            if len(unseen_dags) >= opts.min_aligned_pairs / 2 \
-               or len(unseen_dags) / float(len(these_dags)) > 0.60:
+
+            if len(unseen_dags) >= opts.min_aligned_pairs / 2. \
+                       or len(unseen_dags) / float(len(these_dags)) > 0.60:
                 dags.extend((str(ud) for ud in unseen_dags))
 
         if len(dags) >= opts.min_aligned_pairs:
             header = "#" + "\t".join([str(i), "100.0", a_seqid, b_seqid, direction,
                                  str(len(dags))])
-            print >>fhmeta, header
-            print >>fhmeta, "\n".join(dags)
+            print >>out, header
+            print >>out, "\n".join(dags)
 
 def matches_by_diag_id(matches):
     """ take the structure returned by parse_file and return a dictionary
@@ -324,10 +317,17 @@ def matches_by_diag_id(matches):
     by_diag = collections.defaultdict(list)
     for seqid_pair, accn_pair_dict in matches.iteritems():
         for accn_pair_key, accn_pair in accn_pair_dict.iteritems():
+            assert accn_pair['diag_str'] is not None
             by_diag[accn_pair['diag_str']].append(DagLine.from_pair_dict(accn_pair))
       
     return dict(by_diag) 
 
+def adjust_opts_for_meta(opts):
+    opts.min_aligned_pairs = 1
+    opts.min_score = int(opts.min_aligned_pairs * 0.5 * opts.max_match_score)
+    opts.gap_dist = opts.gap_dist_merge if opts.gap_dist_merge != 0 else 4 * opts.gap_dist 
+    opts.gap_max = opts.gap_max_merge if opts.gap_max_merge != 0 else 5 * opts.gap_max
+    assert 0 < opts.gap_dist < opts.gap_max
 
 if __name__ == "__main__":
     import optparse, sys
@@ -345,9 +345,16 @@ a_seqid<tab>a_accn<tab>a_start<tab>a_end<tab>b_seqid<tab>b_accn<tab>b_start<tab>
                  " below this value")
     p.add_option('-g', dest='gap_dist', type='float', default=10000, 
                 help="averge distance expected between 2 syntenic genes")
+    p.add_option('--gm', dest='gap_dist_merge', type='float', default=0, 
+                help="averge distance expected between 2 syntenic merged genes"
+                " only applicable if --merge is specified")
 
     p.add_option('-D', dest='gap_max', type='float', default=200000,
-                help="maximum distance between 2 matches in basepairs")
+                help="maximum distance between 2 matches")
+    p.add_option('--Dm', dest='gap_max_merge', type='float', default=0, 
+                help="maximum distance expected between 2 syntenic merged genes"
+                " only applicable if --merge is specified")
+
     p.add_option('-E', dest='evalue', type='float', default=1e-5,
                 help="maximum evalue.")
     p.add_option('-A', dest='min_aligned_pairs', type='int', default=6,
@@ -372,22 +379,38 @@ a_seqid<tab>a_accn<tab>a_start<tab>a_end<tab>b_seqid<tab>b_accn<tab>b_start<tab>
                  will contain genes merged into meta groups.
                  """)
 
-    opts, _ = p.parse_args() 
+    # dag file can also be sent in as the first arg.
+    opts, maybe_dag = p.parse_args() 
 
-    if not opts.dag:
+    if not (opts.dag or maybe_dag):
         sys.exit(p.print_help())
+    if not opts.dag: opts.dag = maybe_dag[0]
 
     if opts.min_score is None:
         opts.min_score = int(opts.min_aligned_pairs * 0.5 * opts.max_match_score)
 
 
+    # so here, we run the original dag_chainer. and save to opts.merge.
     all_matches = parse_file(opts.dag, opts.evalue, opts.ignore_dist, merge_genes=False)
-
     out_file = open(opts.merge, 'wb') if opts.merge else sys.stdout
-    meta = iterate_matches(all_matches, opts, out=out_file)
+    run_and_print(all_matches, opts, out=out_file)
+
+
 
 
     if opts.merge:
-        assert len(meta)
-        merge_meta(meta, opts)
+        out_file.close()
+        # then we read that output file, merging the genes.
+        merged_dags = parse_file(opts.merge, opts.evalue, opts.ignore_dist, merge_genes=True)
+
+        # doh! still need to re-parse original. but it's pretty short compared to the original
+        # opts.dag so it's pretty fast.
+        unmerged_dags = parse_file(opts.merge, opts.evalue, opts.ignore_dist, merge_genes=False)
+
+        # then we run and print without printing.
+        adjust_opts_for_meta(opts)
+        # run it to get the meta, but dont print.
+        meta = run_and_print(merged_dags, opts, out=False)
+        out_file = open(opts.merge + ".meta", 'wb') 
+        merge_meta(meta, unmerged_dags, opts, out_file)
 
