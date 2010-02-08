33c59a9b240a81d08d6ff4f784f6ed48ab2da878
bpederse <bpederse@8fdc80c4-b635-0410-baf7-2d8196006633>
2010-02-08 04:58:39 +0000

    fill gaps.

--- trunk/scripts/histone/to_bin.py
+++ trunk/scripts/histone/to_bin.py
@@ -3,6 +3,11 @@ import math
 from pyfasta import Fasta
 import numpy as np
 
+"""
+NOTE: gaps in coverage are filled with the average
+of the value above and the value below.
+"""
+
 hists = (
     "GSM310840_Cy5_Cy3.txt",
     "GSM310841_Cy5_Cy3.txt",
@@ -55,7 +60,6 @@ def parse_hists(fhists):
                 continue
             id, cy5, cy3 = line[0], float(line[1]), float(line[2])
             vals_by_id[id].append((cy5, cy3))
-
     # TODO: see zscore here: for confidence in measurement...
     # http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM310842
     def avg(li): return sum(li)/len(li)
@@ -64,6 +68,53 @@ def parse_hists(fhists):
         cy3 = avg([x[1] for x in c53li])
         yield id, math.log(1 + (cy5 / cy3))
 
+def fill(a):
+    """
+    fill zeros with the preceding value. (dont want moving avg).
+
+    >>> a = np.zeros((12,))
+    >>> a[:3] = 12
+    >>> a[6:9] = 15
+    >>> a[11:] = 13
+
+    >>> fill(a)
+    >>> a
+    array([ 12. ,  12. ,  12. ,  13.5,  13.5,  13.5,  15. ,  15. ,  15. ,
+            14. ,  14. ,  13. ])
+
+    >>> a = np.arange(5)
+    >>> a[0] = 0
+    >>> fill(a)
+    >>> a
+    array([1, 1, 2, 3, 4])
+    >>> a[-1] = 0
+    >>> fill(a)
+    >>> a
+    array([1, 1, 2, 3, 3])
+    """
+    s0, = np.where((a[1:] == 0) & (a[:-1] != 0))
+    s1, = np.where((a[:-1] == 0) & (a[1:] != 0))
+    s0 += 1
+    s1 += 1
+    if len(s0) < len(s1):
+        s0 = np.concatenate(([0], s0))
+    if len(s1) < len(s0):
+        s1 = np.concatenate((s1, [0]))
+        
+    idxs = np.transpose((s0, s1))
+
+    for start, end in idxs:
+        if start == 0:
+            a[start:end] = a[end]
+        elif end == 0:
+            a[start] = a[start - 1]
+        else:
+            a[start:end] = (a[start - 1] + a[end]) / 2
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 if __name__ == "__main__":
     plookups = parse_posns(posns)
@@ -79,6 +130,7 @@ if __name__ == "__main__":
     success = 0
     for id, val in parse_hists(hists):
         start, length = plookups[id]
+        assert val != 0
         #print id, val, start, length
         seqid = id[3]
         if not seqid in "12345": continue
@@ -86,7 +138,6 @@ if __name__ == "__main__":
         counts[seqid][start - 1: start + length] += 1
         c = counts[seqid][start -1: start + length]
     
-
         # keep the average. since there are overlaps, this 
         # weights by the number of existing measurements taht have
         # already contributed to the value.
@@ -94,4 +145,5 @@ if __name__ == "__main__":
         success += 1
     print "ADDED:", success
     for seqid, arr in arrs.iteritems():
+        arr = fill(arr) 
         arr.tofile("histone.%s.bin" % seqid)
